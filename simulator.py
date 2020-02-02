373399143b2dc62de2329f2baac469874251cb74
MUYANG GUO <muyangguo@gmail.com>
2020-02-02 11:58:31 -0500

    update

---
+++ simulator.py
@@ -0,0 +1,35 @@
+import os
+import collections
+import argparse
+import multiprocessing
+import json
+import time
+import numpy as np
+import math as math
+import mpl_toolkits.mplot3d
+import matplotlib
+
+_author_  = 'Muyang Guo, Wei Zhao, Shushu Zhao, Wenyue Wang'
+
+def parseArguments():
+    '''
+    random generator seed: 
+    simulation time:
+
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('seed', help = 'put the seed number for the random number generator', type=float, default = 1)
+    args = parser.parse_args()
+    return args
+
+def main():
+    with open('config.json') as json_config_file:
+        data = json.load(json_config_file)
+    print("Simulation process of {name} starts!\nInitializing with configs ...".format(**data))
+    
+    return
+
+if __name__ == "__main__":
+    args = parseArguments()
+    print("arguments:", args)
+    main() 
